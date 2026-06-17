0fa8ffa1a554a4709b05194a2b07c1138d93e7c9
TOUFIQ <mohamed1806toufiq@gmail.com>
2026-06-17 13:56:11 +0530

    Update test_product_listing.py

--- test_product_listing.py
+++ test_product_listing.py
@@ -9,11 +9,11 @@ def test_product_listing_shows_at_least_one_product():
 
         page.goto(BASE_URL)
 
-        # Select the product grid container
+      
         grid = page.locator("css=div.grid")
         grid.wait_for(state="visible", timeout=5000)
 
-        # Just assert that the grid exists (we know it has products from manual testing)
+       
         assert grid.is_visible()
 
         browser.close()
