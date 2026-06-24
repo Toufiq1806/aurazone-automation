d83178fd76749cd0065d14a903e830dcc1c96d68
TOUFIQ <mohamed1806toufiq@gmail.com>
2026-06-24 07:13:57 +0530

    Update test_cart.py

--- test_cart.py
+++ test_cart.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 BASE_URL = "https://test.aurazone.shop/"
 
-def test_add_to_cart_updates_cart_count():
+def check_title():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch()
         page = browser.new_page()
